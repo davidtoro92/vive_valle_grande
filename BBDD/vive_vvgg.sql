26a19dec95f6aaf8c397422ddcb7b1a20dfb8486
David <david.toro.salamanca@gmail.com>
2018-03-27 21:56:05 -0300

    Respaldo de script de base de datos.

---
+++ BBDD/vive_vvgg.sql
@@ -0,0 +1,12 @@
+CREATE Database vive_valle_grande;
+
+CREATE OR REPLACE TABLE tb_empresa (
+	id int(20) NOT NULL AUTO_INCREMENT,
+	nombre varchar(100) DEFAULT NULL,
+	rubro varchar(100) DEFAULT NULL,
+	pagina_web varchar(100) DEFAULT NULL,
+	tags varchar(100) DEFAULT NULL,
+	descripcion varchar(100) DEFAULT NULL,
+	PRIMARY KEY (id),
+  	KEY (id)
+);t
